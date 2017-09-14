52e9ece7e6c804e6a8a1a0c6a0c59cd6e0658403
Fran <asensi.benito@gmail.com>
2017-09-14 22:04:36 +0200

    Take photo save image and show
    - Take a photo with the camera
    - Save image into de gallery
    - Show image in a image view

--- app/src/main/java/com/example/wolfteqlab/android_takephotoandsavegallery/MainActivity.java
+++ app/src/main/java/com/example/wolfteqlab/android_takephotoandsavegallery/MainActivity.java
@@ -1,54 +1,136 @@
 package com.example.wolfteqlab.android_takephotoandsavegallery;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
 import android.os.Build;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
     public final int REQUEST_CODE_ASK_PERMISSIONS = 1001;
-    private final int requestCode = 20;
+    private static final int REQUEST_TAKE_PHOTO = 1;
+
+    String mCurrentPhotoPath;
+    ImageView ivPreview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ivPreview = (ImageView) findViewById(R.id.imag);
+
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-
-
                 if (Build.VERSION.SDK_INT >= 23) {
                     int hasPermissionCamera = checkSelfPermission(Manifest.permission.CAMERA);
 
                     if (hasPermissionCamera != PackageManager.PERMISSION_GRANTED) {
                         requestPermissions(new String[] {Manifest.permission.CAMERA}, REQUEST_CODE_ASK_PERMISSIONS);
                     } else if (hasPermissionCamera == PackageManager.PERMISSION_GRANTED){
-                        Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                        photoCaptureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
-                                imageUri);
-                        startActivityForResult(photoCaptureIntent, requestCode);
+                        initTakePhoto();
                     }
                 }else {
-                    Intent photoCaptureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    photoCaptureIntent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT,
-                            imageUri);
-                    startActivityForResult(photoCaptureIntent, requestCode);
+                    initTakePhoto();
                 }
             }
         });
     }
 
+    private void initTakePhoto(){
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                return;
+            }
+
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                try {
+                    Uri photoURI = Uri.fromFile(createImageFile());
+                    takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                    startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+                } catch (IOException ex) {
+                    // Error occurred while creating the File
+                    return;
+                }
+
+            }
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM), "Camera");
+
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        mCurrentPhotoPath = "file:" + image.getAbsolutePath();
+        return image;
+    }
+
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if(requestCode != CAMERA_PIC_REQUEST || filename == null)
-            return;
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            // Show the thumbnail on ImageView
+            Uri imageUri = Uri.parse(mCurrentPhotoPath);
+            File file = new File(imageUri.getPath());
+
+            try {
+                InputStream ims = new FileInputStream(file);
+                ivPreview.setImageBitmap(BitmapFactory.decodeStream(ims));
+            } catch (FileNotFoundException e) {
+                return;
+            }
+
+            // ScanFile so it will be appeared on Gallery
+            MediaScannerConnection.scanFile(MainActivity.this,
+                    new String[]{imageUri.getPath()}, null,
+                    new MediaScannerConnection.OnScanCompletedListener() {
+                        public void onScanCompleted(String path, Uri uri) {
+                        }
+                    });
+        }
     }
 }
